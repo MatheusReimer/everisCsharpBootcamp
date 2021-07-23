8b12a656e79c97edb4fbdc2118aa092ac4811cb7
Matheus Reimer <matheusreimer1@gmail.com>
2021-07-23 17:30:34 -0300

    segundo trabalho

---
+++ GameCatalogo/WebApplication1/WebApplication1/Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Exceptions
+{
+    public class JogoNaoCadastradoException : Exception
+    {
+        public JogoNaoCadastradoException()
+
+             : base("Este jogo já está cadastrado")
+        { }
+    }
+}
